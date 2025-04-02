0632925f3e7754f095b887de778f86aa9733eec7
Atonio Velazquez <halconvoladorfenix@gmail.com>
2025-04-02 13:57:37 -0600

    se coloco una funcion

--- hola.java
+++ hola.java
@@ -11,11 +11,31 @@ import java.io.*;
 public class hola{
 
 
-public static void main(String[]args){
+
+
+static void datos(){
 
 
 
 System.out.print("esto es una practica de java see");
+
+
+
+
+}
+
+
+
+
+public static void main(String[]args){
+
+
+
+
+datos();
+
+
+
 
 
 
